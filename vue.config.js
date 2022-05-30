74cb8f7a416acac5ee900f5c4b3f16305ebae176
XeonEVO <43061704+XeonEVO@users.noreply.github.com>
2022-05-30 16:04:10 +0700

    initial commit

--- vue.config.js
+++ vue.config.js
@@ -4,3 +4,13 @@ module.exports = defineConfig({
     'vuetify'
   ]
 })
+
+// vue.config.js file to be placed in the root of your repository
+
+module.exports = {
+  pwa: {
+    workboxOptions: {
+      exclude: [/_redirects/]
+    }
+  }
+}
