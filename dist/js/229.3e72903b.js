86199368de899bc94f28d62f2c84e89095cfc882
XeonEVO <43061704+XeonEVO@users.noreply.github.com>
2022-05-30 15:02:18 +0700

    initial commit

---
+++ dist/js/229.3e72903b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkfrontend"]=self["webpackChunkfrontend"]||[]).push([[229],{5229:function(e,n,t){t.r(n),t.d(n,{default:function(){return f}});var r=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("This is dashboard page.")])])}],l={},s=l,a=t(1001),c=(0,a.Z)(s,r,u,!1,null,null,null),f=c.exports}}]);
+//# sourceMappingURL=229.3e72903b.js.map
